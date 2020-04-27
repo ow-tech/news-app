b18e54a874c70b6929e0502b376fb65b911cba1e
ow-tech <wagwanwekon@outloo.com>
2020-04-27 12:34:05 +0300

    creating view for sources

--- app/views.py
+++ app/views.py
@@ -8,4 +8,15 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    # Getting Sources
+
+    general_news = get_sources('general')
+    business_news = get_sources('business')
+    entertainment_news = get_sources('entertainment')
+    sports_news = get_sources('sports')
+    technology_news = get_sources('technology')
+    science_news = get_sources('science')
+    health_news = get_sources('health')
+
+
+    return render_template('index.html', General=general_news, Business=business_news, Entertainment=entertainment_news, Sports=sports_news, Technology=technology_news, Science=science_news, Health=health_news)
